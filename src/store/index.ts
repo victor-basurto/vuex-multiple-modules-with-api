46fbd64dc2358f62ee0eaded58f73a9b7e984dca
Victor B <vbasurto@newlandco.com>
2021-09-22 14:22:27 -0700

    enabled `logger` to track commits

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,14 @@
-import { createStore } from 'vuex';
+import { createLogger, createStore } from 'vuex';
 import { IRootState } from './interfaces';
 import { RootStoreModuleTypes } from './modules/root/types';
 import { EmployeeStoreModuleTypes } from './modules/employee/types';
 
 import root from './modules/root'
 
-export const store = createStore<IRootState>(root);
+export const store = createStore<IRootState>({
+	...root,
+	plugins: process.env.NODE_ENV === 'development' ? [createLogger()] : []
+});
 
 type StoreModules = {
 	employeeModule: EmployeeStoreModuleTypes;
